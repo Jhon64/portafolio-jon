49c7fe19e2743fc13f3464496252f292bd1da240
Jhon64 <jhtamaranto@gmail.com>
2021-01-27 20:45:11 -0500

    modificando app

--- src/layouts/Header.svelte
+++ src/layouts/Header.svelte
@@ -14,14 +14,14 @@
 
 <header>
   <nav class="nav bg-blue-700 text-white flex fixed top-0 w-full ">
-    <div class=" h-20 flex items-center justify-center w-full">
+    <div class=" h-10 sm:h-20 flex items-center justify-center w-full">
       <label for="icon_menu" class="cursor icon-menu">
         <i class="material-icons  md-36">format list bulleted</i>
       </label>
       <input type="checkbox" on:change={checked} id="icon_menu" class="none" />
     </div>
   </nav>
-  <div id="list-options" class=" fixed top-0 mt-20  list-options px-5">
+  <div id="list-options" class=" fixed top-0 mt-10  list-options px-5">
     <!--opciones de la navegacion-->
     <ul class="list-options-nav">
       <li><a>Home </a></li>
